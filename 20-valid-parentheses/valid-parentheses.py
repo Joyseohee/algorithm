a4a1cdef9baa6e713cfe28532f51bfd6bcd38374
Joyseohee <106529484+Joyseohee@users.noreply.github.com>
2024-06-30 23:24:36 +0900

    Time: 32 ms (77.97%) | Memory: 16.5 MB (48.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,39 +1,19 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        if not self.is_empty():
-            return self.items.pop()
-        else:
-            raise IndexError("pop from empty stack")
-
-    def peek(self):
-        if not self.is_empty():
-            return self.items[-1]
-        else:
-            raise IndexError("peek from empty stack")
-
-    def size(self):
-        return len(self.items)
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = Stack()
-        matching_bracket = {')': '(', '}': '{', ']': '['}
-
-        for char in s:
-            if char in matching_bracket.values():
-                stack.push(char)
-            elif char in matching_bracket.keys():
-                if stack.is_empty() or stack.pop() != matching_bracket[char]:
+        res=[]
+        if(s[0]==")" or s[0]=="}" or s[0]=="]"):
+            return False
+        for i in s:
+            if(i=="(" or i=="{" or i=="["):
+                res.append(i)
+            elif(len(res)!=0 and (res[-1]=="(" or res[-1]=="{" or res[-1]=="[")):
+                if((i==")" and res[-1]=="(") or (i=="}" and res[-1]=="{") or (i=="]" and res[-1]=="[")):
+                    res.pop()
+                else:
                     return False
-
-        return stack.is_empty()
+            else:
+                return False
+        
+        if(len(res)==0):
+            return True
+        return False
