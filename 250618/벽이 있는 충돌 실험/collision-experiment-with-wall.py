5d958e4e61c854151087585dabe6470d1550b398
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-06-18 12:44:45 +0900

    Title: 벽이 있는 충돌 실험, Time: 4084ms, Memory: 29MB, Status: Time Limit Exceed - Codetree

--- 250618/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 250618/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,49 +1,50 @@
 t = int(input())
 
-DIRECTIONS = {
-    "U": (-1, 0),
-    "D": (1, 0),
-    "L": (0, -1),
-    "R": (0, 1)
+direction_map = {
+    'U': (-1, 0),
+    'D': (1, 0),
+    'L': (0, -1),
+    'R': (0, 1)
 }
 
-REVERSE_DIRECTION = {
-    "U": "D",
-    "D": "U",
-    "L": "R",
-    "R": "L"
+reverse_dir = {
+    'U': 'D',
+    'D': 'U',
+    'L': 'R',
+    'R': 'L'
 }
 
-def in_range(x, y, n):
+def in_bounds(x, y, n):
     return 0 <= x < n and 0 <= y < n
 
-def read_marble_input():
-    x, y, d = input().split()
-    return (int(x) - 1, int(y) - 1, d)
-
-def move(marble, n):
-    x, y, d = marble
-    dx, dy = DIRECTIONS[d]
-    nx, ny = x + dx, y + dy
-    if in_range(nx, ny, n):
-        return (nx, ny, d)
-    else:
-        return (x, y, REVERSE_DIRECTION[d])
-
-def remove_collided(marbles, n):
-    grid = [[0] * n for _ in range(n)]
-    for x, y, _ in marbles:
-        grid[x][y] += 1
-    return [m for m in marbles if grid[m[0]][m[1]] == 1]
-
-def simulate(marbles, n):
+def simulate(n, marbles):
     for _ in range(2 * n):
-        marbles = [move(m, n) for m in marbles]
-        marbles = remove_collided(marbles, n)
-    return marbles
+        next_positions = {}
+
+        # move marbles
+        for x, y, d in marbles:
+            dx, dy = direction_map[d]
+            nx, ny = x + dx, y + dy
+
+            if not in_bounds(nx, ny, n):
+                nx, ny = x, y
+                d = reverse_dir[d]
+
+            if (nx, ny) not in next_positions:
+                next_positions[(nx, ny)] = []
+            next_positions[(nx, ny)].append(d)
+
+        # remove duplicates
+        marbles = [
+            (x, y, dirs[0])
+            for (x, y), dirs in next_positions.items()
+            if len(dirs) == 1
+        ]
+    return len(marbles)
 
 for _ in range(t):
     n, m = map(int, input().split())
-    marbles = [read_marble_input() for _ in range(m)]
-    result = simulate(marbles, n)
-    print(len(result))
+    marbles = [tuple(input().split()) for _ in range(m)]
+    marbles = [(int(x)-1, int(y)-1, d) for x, y, d in marbles]
+
+    print(simulate(n, marbles))
