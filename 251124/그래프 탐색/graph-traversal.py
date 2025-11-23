7f9f49f90832dc4062b3cc116745bdeb8234ef4e
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-11-24 07:43:08 +0900

    Title: 그래프 탐색, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251124/그래프 탐색/graph-traversal.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+graph = [[] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+# Please write your code here.
+
+for a, b in edges:
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(vertex):
+    for node in graph[vertex]:
+        if visited[node] == 0:
+            visited[node] = 1
+            dfs(node)
+
+dfs(1)
+print(sum(visited) - 1)
