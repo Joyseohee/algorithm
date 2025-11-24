e07dcf9fecde5834b73d2e39ad227b678d6215ea
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-11-24 21:00:02 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.90%) - LeetSync

--- leetcode/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ leetcode/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,26 +1,15 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = [0 for _ in range(n + 1)]
-
-        def tribo(n):
-            if n == 0:
-                return 0
-            if n <= 2:
-                return 1
-
-            t3 = memo[n-3] if memo[n-3] > 0 else tribo(n-3)
-            t2 = memo[n-2] if memo[n-2] > 0 else tribo(n-2)
-            t1 = memo[n-1] if memo[n-1] > 0 else tribo(n-1)
-
-            t = t3 + t2 + t1
-            
-            memo[n] = t
-
-            return t
-
-        return tribo(n)
-
-
+        # T0, T1, T2 직접 처리
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
         
-
+        t0, t1, t2 = 0, 1, 1  # T0, T1, T2
         
+        # i: 3 ~ n
+        for _ in range(3, n + 1):
+            t0, t1, t2 = t1, t2, t0 + t1 + t2
+        
+        return t2
