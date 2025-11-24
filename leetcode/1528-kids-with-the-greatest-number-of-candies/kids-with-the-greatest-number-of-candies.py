bcda7f5d701c22b7a51720741fc5c78acf6bdb20
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-11-24 23:25:09 +0900

    Time: 3 ms (14.72%) | Memory: 17.8 MB (53.81%) - LeetSync

--- leetcode/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ leetcode/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,15 +1,11 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        n = len(candies)
-        is_gretest = [False for _ in range(n)]
+        result = []
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= max(candies):
+                result.append(True)
+            else:
+                result.append(False)
 
-        # 사탕의 최댓값
-        max_candy = max(candies)
-
-        # 배열 순회하면서 extra를 더했을 때 > 최대값보다 큰지
-        for idx, candy in enumerate(candies):
-            if candy + extraCandies >= max_candy:
-                is_gretest[idx] = True
-        
-        return is_gretest      
+        return result
         
