ccfa5c87b4dce675a44639369d7f4d315ced9ff3
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-11-24 23:57:13 +0900

    Time: 7 ms (73.94%) | Memory: 18.2 MB (35.21%) - LeetSync

--- leetcode/605-can-place-flowers/can-place-flowers.py
+++ leetcode/605-can-place-flowers/can-place-flowers.py
@@ -1,29 +1,24 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+
         length = len(flowerbed)
-        planted_count = 0
 
-        if length == 1:
-            planted_count = 1 if flowerbed[0] == 0 else 0
-            return planted_count >= n
-
-        # 화단을 순회하면서 양 옆에 인접한 꽃이 없다면 꽃을 추가
         for i in range(length):
             if flowerbed[i] == 1:
                 continue
-            
-            if i == 0 and flowerbed[1] == 0:
-                flowerbed[i] = 1
-                planted_count += 1
 
-            elif i == length - 1 and flowerbed[length - 2] == 0:
-                flowerbed[i] = 1
-                planted_count += 1
+            left_empty = (i == 0) or (flowerbed[i - 1] == 0)
+            right_empty = (i == length - 1) or (flowerbed[i + 1] == 0)
 
-            elif flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+            if left_empty and right_empty:
                 flowerbed[i] = 1
-                planted_count += 1
-            
-        return planted_count >= n
+                n -= 1
+                
+                if n == 0:
+                    return True
+
+        return False
 
         
