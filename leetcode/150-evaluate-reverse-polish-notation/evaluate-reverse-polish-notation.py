e41f52fb2e5b302d0e57ab393ba0b034a03b76d6
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-12-04 14:11:40 +0900

    Time: 7 ms (14.13%) | Memory: 19.1 MB (51.19%) - LeetSync

--- leetcode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ leetcode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,7 +1,6 @@
 class Solution:
     
     def evalRPN(self, tokens: List[str]) -> int:
-
         def calculator(a: int, b: int, calc: str) -> int:
             if calc == '+':
                 return a + b
@@ -12,8 +11,11 @@ class Solution:
             if calc == '/':
                 return int(a / b)
 
-
         cal_set = set(['+', '-', '*', '/'])
+
+        for i in range(len(tokens)):
+            tokens[i] = int(tokens[i]) if tokens[i] not in cal_set else tokens[i]
+
         new_tokens = []
 
         while len(tokens) > 0:
@@ -21,8 +23,8 @@ class Solution:
                 new_tokens.append(tokens.pop())
             else:
                 if new_tokens and new_tokens[-1] not in cal_set:
-                    number1 = int(tokens.pop())
-                    number2 = int(new_tokens.pop())
+                    number1 = tokens.pop()
+                    number2 = new_tokens.pop()
                     calc = new_tokens.pop()
 
                     cal_result = calculator(number1, number2, calc)
@@ -32,7 +34,4 @@ class Solution:
                 else:
                     new_tokens.append(tokens.pop())
 
-            # print(f"tokens={tokens}, new_tokens={new_tokens}")
-
-
-        return int(new_tokens[0])
+        return new_tokens[0]
