a1f8faa91caa0ba8851b14d8c30226c842984c68
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2025-12-05 21:43:39 +0900

    Time: 4 ms (29.36%) | Memory: 19.1 MB (87.47%) - LeetSync

--- leetcode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ leetcode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,21 +1,34 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operands = {
-            "+": lambda a, b: a + b,
-            "-": lambda a, b: b - a,
-            "*": lambda a, b: a * b,
-            "/": lambda a, b: int(b/a)
-        }
-
+        # 스택
         stack = []
-        total = 0
-        for x in tokens:
-            if x not in operands:
-                stack.append(x)
+
+        # 연산자 정의 : set
+        operator = set(["+", "-", "*", "/"])        
+
+        # 연산 계산기
+        def calculator(calc, a, b):
+            if calc == "+":
+                return a + b
+            if calc == "-":
+                return a - b
+            if calc == "*":
+                return a * b
+            if calc == "/":
+                return int(a / b)
+
+        # tokens 순회 숫자 stack에 넣기, 연산자가 등장하면 숫자를 꺼내서 연산 -> stack
+        for c in tokens:
+            if c not in operator:
+                stack.append(int(c))
             else:
-                i1 = int(stack.pop())
-                i2 = int(stack.pop())
-                total = operands[x](i1, i2)
+                num1 = stack.pop()
+                num2 = stack.pop()
+                calc = c
+                total = calculator(c, num2, num1)
                 stack.append(total)
 
-        return int(stack[0])
+        # stack 1개의 숫자
+        return stack[0]
+        
+        
