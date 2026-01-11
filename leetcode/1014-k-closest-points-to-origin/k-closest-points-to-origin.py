99e42af9faf551cb59247e27cf270be833e28e8b
Seohee Park <106529484+Joyseohee@users.noreply.github.com>
2026-01-12 07:16:46 +0900

    Time: 71 ms (51.71%) | Memory: 24.7 MB (6.04%) - LeetSync

--- leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,12 +1,17 @@
+import heapq
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # x+y값이 가장 작은 거 k개
-        # 정렬 알고리즘 O(NlogN) 10^4*log2_10^4 -> 10^5 -> 안정적
+        k_heap = []
 
-        points.sort(key = lambda x: x[0] ** 2 + x[1] ** 2)
-        # print(points)
+        for x, y in points:
+            dist = - (x ** 2 + y ** 2)
+            heapq.heappush(k_heap, (dist, [x, y]))
 
-        return points[:k]
+            if len(k_heap) > k:
+                heapq.heappop(k_heap)
+
+        return [point for dist, point in k_heap]
 
 
         
